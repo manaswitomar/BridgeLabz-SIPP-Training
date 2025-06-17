765c0328b54a49dab2d6bbd42b05e6740524a849
manaswitomar <manaswitomar17@gmail.com>
2025-06-17 10:30:22 +0530

    eligible to vote

--- Day-3_ControlFlows/Vote.java
+++ Day-3_ControlFlows/Vote.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfNaturalNumbers {
+public class Vote{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
