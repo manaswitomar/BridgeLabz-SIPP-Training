b532c0edf1481c788fe50116b3439c1940680080
manaswitomar <manaswitomar17@gmail.com>
2025-06-17 10:31:31 +0530

    calculate the sum of natural numbers

--- Day-3_ControlFlows/Natural_number.java
+++ Day-3_ControlFlows/Natural_number.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfNaturalNumbers {
+public class NaturalNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
