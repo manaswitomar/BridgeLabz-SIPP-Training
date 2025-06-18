edbb6c5806d0d0efb02886f742f0139172bfa394
manaswitomar <manaswitomar17@gmail.com>
2025-06-18 11:35:09 +0530

    [manaswi] Added Day2 control flow programs

---
+++ Day2_ControlFlowPrograms/Multiple_Below.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DivisorsBelow100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer less than 100: ");
+        int number = scanner.nextInt();
+        if (number > 0 && number < 100) {
+            int counter = number - 1;
+
+            System.out.println("Numbers less than " + number + " that divide it exactly:");
+            while (counter > 1) {
+                
+                if (number % counter == 0) {
+                    
+                    System.out.println(counter);
+                }
+                counter--;
+            }
+        } else {
+            System.out.println("Please enter a valid positive integer less than 100.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ Day2_ControlFlowPrograms/Smallest_Number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt(); 
+        int n2 = sc.nextInt(); 
+        int n3 = sc.nextInt(); 
+        if(n1<n2 && n1<n3){
+            System.out.println("Is the first number the smallest?yes");
+        }
+        else if(n2<n1 && n2<n3){
+            System.out.println("Is the second number the smallest?yes");
+
+        }else{
+            System.out.println("Is the third number the smallest?yes");
+        }
+    }
+}
