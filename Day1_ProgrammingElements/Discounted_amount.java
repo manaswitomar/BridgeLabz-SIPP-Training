3b3f9f785651b8c1aa1b27992e7da296f6f0a14a
manaswitomar <manaswitomar17@gmail.com>
2025-06-18 10:15:01 +0530

    [manaswi] Programming Elements assignment

---
+++ Day1_ProgrammingElements/Discounted_amount.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args){
+        int fee=125000;
+        double  discountPercent =0.10;
+        double discount=discountPercent*fee;
+        int fee_paid=fee-discount;
+        System.out.println("The discount amount is INR"+discount+" and final discounted fee is INR"+fee_paid);
+
+
+    }
+}
