265788ae897a4f53176d81cf3f64e81d76633f58
manaswitomar <manaswitomar17@gmail.com>
2025-06-19 21:34:40 +0530

    generate null pinter exception

---
+++ Day4_Strings/NullPointer.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class NullPointer {
+    public static void generateNullPointerException() {
+        String text = null;
+        int length = text.length(); 
+        System.out.println("Length: " + length); 
+    }
+    public static void handleNullPointerException() {
+        String text = null;
+        try {
+            int length = text.length(); 
+            System.out.println("Length: " + length);
+        } catch (NullPointerException e) {
+            System.out.println( e.getMessage());
+        } catch (RuntimeException e) {
+            System.out.println( e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+
+        System.out.println("--- Generating Unhandled NullPointerException ---");
+        try {
+            generateNullPointerException();
+        } catch (Exception e) {
+            System.out.println("Program crashed due to unhandled exception: " + e);}
+        System.out.println("\n--- Handling NullPointerException with Try-Catch ---");
+        handleNullPointerException();
+    }
+}
