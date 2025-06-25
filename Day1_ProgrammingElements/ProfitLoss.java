9494758d833db26d3b8ea8e4553a59c42b2c7ec3
Manaswi Tomar <158082269+manaswitomar@users.noreply.github.com>
2025-06-25 22:49:24 +0530

    Delete Day1_ProgrammingElements directory

--- Day1_ProgrammingElements/ProfitLoss.java
+++
@@ -1,9 +0,0 @@
-class ProfitLoss{
-    public static void main(){
-        int Cost_Price=129;
-        int Selling_Price=191;
-            int profit=Selling_Price-Cost_Price;
-            double profitPercentage=(profit/Cost_Price)*100;
-        System.out.println("The cost price is"+Cost_Price+"and selling price is"+Selling_Price+"The profit is"+profit+"and profitPercentage is"+profitPercentage);
-    }
-}
