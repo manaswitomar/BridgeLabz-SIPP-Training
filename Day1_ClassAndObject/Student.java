9075fc02bf67b6e73fc67288f1683cdf92c94fc5
manaswitomar <manaswitomar17@gmail.com>
2025-06-25 23:02:54 +0530

    [Manaswi Tomar] add. Class and Object code Student added

---
+++ Day1_ClassAndObject/Student.java
@@ -0,0 +1,39 @@
+public class Student{
+    private String name;
+    private int rollNumber;
+    private double marks;
+
+    public Student(String name, int rollNumber, double marks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.marks = marks;
+    }
+    public String calculateGrade() {
+        if (marks >= 90) {
+            return "A";
+        } else if (marks >= 80) {
+            return "B";
+        } else if (marks >= 70) {
+            return "C";
+        } else if (marks >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + calculateGrade());
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Mansi", 101, 88.5);
+        Student student2 = new Student("Abhay", 102, 59.0);
+
+        student1.displayDetails();
+        student2.displayDetails();
+    }
+}
+
