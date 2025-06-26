01d547f1aa92894efa886e6a55796af42bd31727
manaswitomar <manaswitomar17@gmail.com>
2025-06-26 13:16:42 +0530

    [Manaswi Tomar] add. class and object code BankAccount added

--- Day1_ClassAndObject/BankAccount.java
+++ Day1_ClassAndObject/BankAccount.java
@@ -3,6 +3,7 @@ public class BankAccount {
     private String accountNumber;
     private double balance;
 
+
     public BankAccount(String accountHolder, String accountNumber, double balance) {
         this.accountHolder = accountHolder;
         this.accountNumber = accountNumber;
