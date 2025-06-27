280f3d353f3160b7e5433980c520d2deb7211940
manaswitomar <manaswitomar17@gmail.com>
2025-06-27 22:26:17 +0530

    [Manaswi Tomar] add. java keywords code EmployeeManagementSystem added

---
+++ Day3_JavaKeywords/EmployeeManagementSystem.java
@@ -0,0 +1,28 @@
+public class EmployeeManagementSystem {
+    static String companyName;
+    String name;
+    final int id;
+    String designation;
+
+    EmployeeManagementSystem(String companyName, String name, int id,String designation){
+        this.companyName=companyName;
+        this.name=name;
+        this.id=id;
+        this.designation=designation;
+    }
+
+
+    public void displayTotalEmployee(){
+        if(this instanceof EmployeeManagementSystem)
+        System.out.println("Company Name :"+companyName);
+        System.out.println("Name :"+name);
+        System.out.println("id :"+ id);
+        System.out.println("Designation :"+ designation);
+    }
+
+    public static void main(String[] args) {
+        EmployeeManagementSystem employee=new EmployeeManagementSystem("Amway","Mansi", 121, "Manager");
+        employee.displayTotalEmployee();
+    }
+
+}
