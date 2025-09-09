9e40fbe25138d4e1f3de4120dece7caa45c0a4b0
manaswitomar <manaswitomar17@gmail.com>
2025-09-09 20:32:38 +0530

    [manaswi tomar] add. java generics code smart warehouse added

---
+++ Java_Generics/SmartWarehouse.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+abstract class WarehouseItem {
+    private String name;
+
+    public WarehouseItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void displayInfo();
+}
+
+class Electronics extends WarehouseItem {
+    public Electronics(String name) {
+        super(name);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Electronics Item: " + getName());
+    }
+}
+
+class Groceries extends WarehouseItem {
+    public Groceries(String name) {
+        super(name);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Grocery Item: " + getName());
+    }
+}
+
+class Furniture extends WarehouseItem {
+    public Furniture(String name) {
+        super(name);
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Furniture Item: " + getName());
+    }
+}
+
+class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
+
+class WarehouseUtils {
+    public static void displayAllItems(List<? extends WarehouseItem> items) {
+        for (WarehouseItem item : items) {
+            item.displayInfo();
+        }
+    }
+}
+
+public class SmartWarehouse {
+    public static void main(String[] args) {
+
+        Storage<Electronics> electronicsStorage = new Storage<>();
+        Storage<Groceries> groceriesStorage = new Storage<>();
+        Storage<Furniture> furnitureStorage = new Storage<>();
+
+        electronicsStorage.addItem(new Electronics("Laptop"));
+        electronicsStorage.addItem(new Electronics("Smartphone"));
+
+        groceriesStorage.addItem(new Groceries("Rice"));
+        groceriesStorage.addItem(new Groceries("Milk"));
+
+        furnitureStorage.addItem(new Furniture("Chair"));
+        furnitureStorage.addItem(new Furniture("Table"));
+
+        System.out.println("=== Electronics Storage ===");
+        WarehouseUtils.displayAllItems(electronicsStorage.getItems());
+
+        System.out.println("\n=== Groceries Storage ===");
+        WarehouseUtils.displayAllItems(groceriesStorage.getItems());
+
+        System.out.println("\n=== Furniture Storage ===");
+        WarehouseUtils.displayAllItems(furnitureStorage.getItems());
+    }
+}
