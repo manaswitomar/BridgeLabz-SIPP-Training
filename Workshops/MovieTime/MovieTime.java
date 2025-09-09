2ef6f0188a2d3c9ac5d5bffd85141b896de97ecd
manaswitomar <manaswitomar17@gmail.com>
2025-09-09 15:45:41 +0530

    [manaswi] add movie time added

--- Workshops/MovieTime/MovieTime.java
+++ Workshops/MovieTime/MovieTime.java
@@ -2,22 +2,22 @@ package MovieTime;
 
 import java.util.*;
 
-public class MovieTime {
-    public static void main(String[] args) {
+public class MovieTime{
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("movie time");
         int[] showtime = new int[0];
         int newShowtime = sc.nextInt();
         int[] updatedShowtimes = new int[showtime.length + 1];
-        for (int i = 0; i < showtime.length; i++) {
+        for (int i = 0; i < showtime.length; i++){
             updatedShowtimes[i] = showtime[i];
         }
         updatedShowtimes[updatedShowtimes.length - 1] = newShowtime;
 
-        for (int i = 1; i < updatedShowtimes.length; i++) {
+        for (int i = 1; i < updatedShowtimes.length; i++){
             int key = updatedShowtimes[i];
             int j = i - 1;
-            while (j >= 0 && updatedShowtimes[j] > key) {
+            while (j >= 0 && updatedShowtimes[j] > key){
                 updatedShowtimes[j + 1] = updatedShowtimes[j];
                 j--;
             }
@@ -29,4 +29,3 @@ public class MovieTime {
         }
     }
 }
-
